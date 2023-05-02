4c44e9c01ee8299b9d02972b859db6151a681698
Stead08 <inomatama08@gmail.com>
2023-05-02 20:11:15 +0900

    add deno fresh project and rust main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,55 @@
-use axum::{routing::get, Router};
+mod router;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+use axum::extract::FromRef;
+use axum_extra::extract::cookie::Key;
+use shuttle_secrets::SecretStore;
+use sqlx::PgPool;
+use crate::router::create_router;
+
+
+#[derive(Clone)]
+pub struct AppState {
+    postgres: PgPool,
+    key: Key,
+    smtp_email: String,
+    smtp_password: String,
+    domain: String,
+}
+
+impl FromRef<AppState> for Key {
+    fn from_ref(state: &AppState) -> Self {
+        state.key.clone()
+    }
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+async fn axum(
+    #[shuttle_shared_db::Postgres] postgres: PgPool,
+    #[shuttle_secrets::Secrets] secrets: SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!().run(&postgres).await.expect("Failed to run migrations!");
+
+    let smtp_email = secrets
+        .get("SMTP_EMAIL")
+        .expect("You need to set your SMTP_EMAIL secret!");
+
+    let smtp_password = secrets
+        .get("SMTP_PASSWORD")
+        .expect("You need to set your SMTP_PASSWORD secret!");
+
+    let domain = secrets
+        .get("DOMAIN")
+        .expect("You need to set your DOMAIN secret!");
+
+    let state = AppState {
+        postgres,
+        key: Key::generate(),
+        smtp_email,
+        smtp_password,
+        domain,
+    };
+
+    let router =create_router(state);
 
     Ok(router.into())
 }
