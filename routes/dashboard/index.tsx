225cc28ae6e272ac75abbc2ae2d48594bf690579
Stead08 <inomatama08@gmail.com>
2023-05-19 11:27:00 +0900

    for deploy

--- routes/dashboard/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-  return (
-    <>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <h1 class="text-2xl font-bold">Dashboard</h1>
-      </div>
-    </>
-  );
-}
